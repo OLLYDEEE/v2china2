d1b753e0b924c09278871244e49d80ffb2a2f474
innocentdavid <paulinnocent04@gmail.com>
2024-03-11 16:17:33 +0100

    jjj

---
+++ src/component/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen  ">
+      <div className="w-full justify-center items-center flex">
+        <h1 className=" items-center text-[96px] font-montserrat inline-block text-center font-[500] ">
+          Dominate the Chinese <br />
+          Market with<span className="font-SF text-[#F13F3F]"> Few Words</span>
+        </h1>
+        <p className="text-[20px] text-[#0ff]">hello</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
